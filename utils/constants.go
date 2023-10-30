face51cf6174f033a31d84f022c9e6e2cc990697
MalteHerrmann <42640438+MalteHerrmann@users.noreply.github.com>
2023-10-30 13:00:40 +0100

    imp: Reduce delta height (#22)
    * move delta height to constants
    
    * reduce delta height to 10 blocks

--- utils/constants.go
+++ utils/constants.go
@@ -7,6 +7,8 @@ import (
 const (
 	// The amount of fees to be sent with a default transaction.
 	defaultFees int = 1e18 // 1 aevmos
+	// The amount of blocks in the future that the upgrade will be scheduled.
+	DeltaHeight = 10
 	// The denomination used for the local node.
 	denom = evmosutils.BaseDenom
 )
